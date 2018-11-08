9504d287f1433beffabe43ef427e0fa15a97f364
Pietro <pietro.balzan@studenti.unipd.it>
2018-11-08 09:49:03 +0100

    Empty App

---
+++ app/src/main/java/ocrcamera/se18/unipd/ocr_comparison/ComparisonActivity.java
@@ -0,0 +1,13 @@
+package ocrcamera.se18.unipd.ocr_comparison;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComparisonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comparison);
+    }
+}
