16d8a98f7d5cf53ff5404d093c0f2e33920010a2
NicolòCervo <nicolo.cervo@studenti.unipd.it>
2018-11-09 16:49:24 +0100

    added java file with ingredientsTextComparison method

--- app/src/main/java/ocrcamera/se18/unipd/ocr_comparison/ComparisonActivity.java
+++ app/src/main/java/ocrcamera/se18/unipd/ocr_comparison/ComparisonActivity.java
@@ -1,7 +1,18 @@
 package ocrcamera.se18.unipd.ocr_comparison;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class ComparisonActivity extends AppCompatActivity {
 
@@ -9,5 +20,44 @@ public class ComparisonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comparison);
+
+        ImageView image = findViewById(R.id.image);
+        TextView text =findViewById(R.id.text);
+
+        Bitmap myBitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getAbsolutePath()+"foto1.jpg");
+        image.setImageBitmap(myBitmap);
+
+
+/*
+        String filenameTxt = "foto1.txt";
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+filenameTxt;
+        String ingredients = null;
+        try {
+            ingredients = getStringFromFile(filePath);
+        }catch(Exception e) {}
+
+        text.setText(ingredients);
+
+    }
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    public static String getStringFromFile (String filePath) throws Exception {
+        File fl = new File(filePath);
+        FileInputStream fin = new FileInputStream(fl);
+        String ret = convertStreamToString(fin);
+        //Make sure you close all streams.
+        fin.close();
+        return ret;
+    }
+    */
     }
 }
